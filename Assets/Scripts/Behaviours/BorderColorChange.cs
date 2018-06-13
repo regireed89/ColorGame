94b5997d85bfe5710b04f5485bf622548ea2e878
regireed89 <regireed89@gmail.com>
2018-06-13 17:59:15 -0500

    ok

--- Assets/Scripts/Behaviours/BorderColorChange.cs
+++ Assets/Scripts/Behaviours/BorderColorChange.cs
@@ -2,54 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BorderColorChange : MonoBehaviour {
+public class BorderColorChange : MonoBehaviour
+{
 
     private Colors color;
     public FloatVariable BorderResetTime;
     public FloatVariable BorderColorChangeTimer;
-    public BoolVariable Collect;
-    public GameEvent OnBorderChange;
+    public GameEvent ChangeBorder;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         color = FindObjectOfType<Colors>();
         BorderColorChangeTimer.Value = BorderResetTime.Value;
         var rColor = Random.Range(0, color.colors.Count);
         gameObject.GetComponent<SpriteRenderer>().color = color.colors[rColor];
-        var c = Random.Range(0, 1);
-        if (c > 0)
-            Collect.Value = true;
-        else
-            Collect.Value = false;
-
-
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         BorderColorChangeTimer.Value -= Time.deltaTime;
         if (BorderColorChangeTimer.Value <= 0)
         {
-            OnBorderChange.Raise();
-            BorderColorChangeTimer.Value = BorderResetTime.Value;
-            var rColor = Random.Range(0, color.colors.Count);
-            gameObject.GetComponent<SpriteRenderer>().color = color.colors[rColor];
-            var c = Random.Range(0, 1);
-            if (c > 0)
-                Collect.Value = true;
-            else
-                Collect.Value = false;
+            ChangeBorder.Raise();
         }
     }
 
-    public void BorderChanged()
+    public void ChangeBorderColor()
     {
         foreach (var c in FindObjectsOfType<CollectibleBehaviour>())
         {
-            c.gameObject.GetComponent<SphereCollider>().enabled = false;
+            if (c.gameObject.GetComponent<CollectibleBehaviour>().data.Color == GetComponent<SpriteRenderer>().color)
+                return;
         }
+        BorderColorChangeTimer.Value = BorderResetTime.Value;
+        var rColor = Random.Range(0, color.colors.Count);
+        gameObject.GetComponent<SpriteRenderer>().color = color.colors[rColor];
     }
 
 }
